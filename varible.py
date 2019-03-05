d57e22b12540693fbd691dab00478ef8df3d80bd
JNYXR <46438362+JNYXR@users.noreply.github.com>
2019-03-05 11:15:53 +0800

    Create varible.py

---
+++ varible.py
@@ -0,0 +1,7 @@
+Gb_batch_size = 16
+Gb_learning_rate = 0.0000001
+Gb_ckpt_dir = '/media/hsq/新加卷/ubuntu/ckpt/vgg16/0'
+Gb_epoch = 100
+Gb_save_frequency = 500
+
+Gb_data_dir = 'D:/DeepLearning/data2/birds/images/'
